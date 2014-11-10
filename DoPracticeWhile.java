9258190f5c3b2b20bf2bbb032350acf9f8c922b4
unknown <smayer04@CSPC060.dcs.bbk.ac.uk>
2014-11-10 20:47:32 +0000

    Added class 'DoPracticeWhile.java'.

---
+++ DoPracticeWhile.java
@@ -0,0 +1,30 @@
+public class DoPracticeWhile {
+    public static void main(String[] args) {
+        int students = 0;
+        int mark = 0;
+        int distinctions = 0;
+        int passes = 0;
+        int fails = 0;
+        int invalids = 0;
+        
+        do {
+            System.out.print("Input a mark: ");
+            mark = Integer.parseInt(System.console().readLine());
+            if (mark < -1 || mark > 100) {
+                invalids++;
+            } else if (mark >= 70) {
+                distinctions++;
+                students++;
+            } else if (mark >= 50) {
+                passes++;
+                students++;
+            } else if (mark != -1) {
+                fails++;
+                students++;
+            }
+        } while (mark != -1);
+        
+        System.out.println("There are " + students + " students: " + distinctions + " distinctions, "
+                                        + passes + " passes, " + fails + " fails (plus " + invalids + " invalid).");
+    }
+}
