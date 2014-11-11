19fb735a2f8a5caede2b9208603bc8a2f9c092e7
unknown <stefanm@LONWRK07474.eidos.com>
2014-11-11 15:48:37 +0000

    Created class 'Patient'.

---
+++ Patient.java
@@ -0,0 +1,39 @@
+public class Patient {
+    private String name;
+    private int age;
+    private String illness;
+    private Patient nextPatient;
+
+    public Patient(String name, int age, String illness) {
+        this.name = name;
+        this.age = age;
+        this.illness = illness;
+        this.nextPatient = null;
+    }
+
+    public void addPatient(Patient newPatient) {
+        if (nextPatient == null) {
+            nextPatient = newPatient;
+        } else {
+            nextPatient.addPatient(newPatient);
+        }
+    }
+
+    public boolean deletePatient(Patient patient) {
+        if (nextPatient == null) {
+            return false;
+        } else if (nextPatient.name.equals(patient.name)) {
+            nextPatient = nextPatient.nextPatient;
+            return true;
+        } else {
+            return nextPatient.deletePatient(patient);
+        }
+    }
+
+    public void printPatients() {
+        System.out.println("Patient: " + name + ", age: " + age + ", illness: " + illness);
+        if (nextPatient != null) {
+            nextPatient.printPatients();
+        }
+    }
+}
